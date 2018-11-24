205d5acfd1f3592cb7e0eaa2ed6f9980327e15cf
Gerbert Santos <gerbert.nsantos@gmail.com>
2018-11-24 18:06:50 -0200

    Criação de projetos

---
+++ site/portal03/SiteMvc/SiteMvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SiteMvc.Models;
+
+namespace SiteMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromForm] Usuario usuario)
+        {
+            if (ModelState.IsValid) { 
+                if (usuario.Email == "teste@app.com" && usuario.Senha == "123456")
+                {
+                    return RedirectToAction("Index", "Palavras");
+                }
+                else
+                {
+                    ViewBag.Message = "Login ou senha inválidos.";
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ site/portal03/SiteMvc/SiteMvc/Controllers/PalavrasController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SiteMvc.Models;
+
+namespace SiteMvc.Controllers
+{
+    public class PalavrasController : Controller
+    {
+        //Listar todos os regirtros do banco de dados
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Criar o CRUD
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm] Palavra palavra)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Atualizar([FromForm] Palavra palavra)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            return Redirect("Index");
+        }
+
+    }
+}
